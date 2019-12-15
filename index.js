5769696feccda4a61201a8d88e8b20994086dfd2
弓削隼大 <18440@toba.kosen-ac.jp>
2019-12-15 16:16:57 +0900

    ガラッと変えた

--- index.js
+++ index.js
@@ -1,42 +1,90 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const line = require("@line/bot-sdk");
+'use strict';
 
-const config = {
-  channelAccessToken: process.env.ACCESS_TOKEN,
-  channelSecret: process.env.SECRET_KEY
+const http = require('http');
+const https = require('https');
+const crypto = require('crypto');
+
+const HOST = 'api.line.me'; 
+const REPLY_PATH = '/v2/bot/message/reply';//リプライ用
+const CH_SECRET = process.env.ACCESS_TOKEN; //Channel Secretを指定
+const CH_ACCESS_TOKEN = process.env.SECRET_KEY; //Channel Access Tokenを指定
+const SIGNATURE = crypto.createHmac('sha256', CH_SECRET);
+const PORT = 3000;
+
+/**
+ * httpリクエスト部分
+ */
+const client = (replyToken, SendMessageObject) => {    
+    let postDataStr = JSON.stringify({ replyToken: replyToken, messages: SendMessageObject });
+    let options = {
+        host: HOST,
+        port: 443,
+        path: REPLY_PATH,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'X-Line-Signature': SIGNATURE,
+            'Authorization': `Bearer ${CH_ACCESS_TOKEN}`,
+            'Content-Length': Buffer.byteLength(postDataStr)
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        let req = https.request(options, (res) => {
+                    let body = '';
+                    res.setEncoding('utf8');
+                    res.on('data', (chunk) => {
+                        body += chunk;
+                    });
+                    res.on('end', () => {
+                        resolve(body);
+                    });
+        });
+
+        req.on('error', (e) => {
+            reject(e);
+        });
+        req.write(postDataStr);
+        req.end();
+    });
 };
 
-const client = new line.Client(config);
+http.createServer((req, res) => {    
+    if(req.url !== '/' || req.method !== 'POST'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('');
+    }
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .post("/hook/", (req, res) => lineBot(req, res))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });        
+    req.on('end', () => {
+        if(body === ''){
+          console.log('bodyが空です。');
+          return;
+        }
 
-function lineBot(req, res) {
-  res.status(200).end();
-  const events = req.events;
-  const promises = [];
-  console.log(req);
-  for (let i = 0, l = events.length; i < l; i++) {
-    const ev = events[i];
-    promises.push(
-      echoman(ev)
-    );
-  }
-  Promise.all(promises).then(console.log("pass"));
-}
+        let WebhookEventObject = JSON.parse(body).events[0];        
+        //メッセージが送られて来た場合
+        if(WebhookEventObject.type === 'message'){
+            let SendMessageObject;
+            if(WebhookEventObject.message.type === 'text'){
+                SendMessageObject = [{
+                    type: 'text',
+                    text: WebhookEventObject.message.text
+                }];
+            }
+            client(WebhookEventObject.replyToken, SendMessageObject)
+            .then((body)=>{
+                console.log(body);
+            },(e)=>{console.log(e)});
+        }
 
-async function echoman(ev) {
-  const pro =  await client.getProfile(ev.source.userId);
-  return client.replyMessage(ev.replyToken, {
-    type: "text",
-    text: "${ev.message.text}"
-  })
-}
-  
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('success');
+    });
+
+}).listen(PORT);
+
+console.log(`Server running at ${PORT}`);
